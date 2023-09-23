4e3e172983609491e5a9396418b2b5627c8d19d9
Anderson <anderson.fagundes4@gmail.com>
2023-09-23 23:44:23 +0100

    Created the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { Header } from './components/Header';
+import './global.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Header />
+
       <h1>Hello</h1>
     </>
   );
