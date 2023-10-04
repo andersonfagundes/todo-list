a397fb47e1a86326001273da318741256fd6c3ec
Anderson <anderson.fagundes4@gmail.com>
2023-10-04 14:13:40 +0100

    Implemented the line-through style in the label

--- src/components/TaskCreated.jsx
+++ src/components/TaskCreated.jsx
@@ -1,15 +1,30 @@
 import styles from './TaskCreated.module.css';
 import trash from '../assets/trash.svg';
+import React, { useState } from 'react';
 
 export function TaskCreated({ task, onDeleteTask }) {
+  const [isRadioChecked, setIsRadioChecked] = useState(false);
+
   function handleDeleteTask() {
     onDeleteTask(task);
   }
 
+  function handleCheckTask(event) {
+    const isChecked = event.target.checked;
+    setIsRadioChecked(isChecked);
+  }
+
+  const labelStyle = isRadioChecked ? { textDecoration: 'line-through' } : {};
+
   return (
     <article>
-      <input type="radio" />
-      <label>{task.content}</label>
+      <input
+        type="radio"
+        name="taskStatus"
+        className={styles.checkedTask}
+        onClick={handleCheckTask}
+      />
+      <label style={labelStyle}>{task.content}</label>
       <img src={trash} onClick={handleDeleteTask} />
     </article>
   );
